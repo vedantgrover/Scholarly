9c39cdc38689f39d40dc1ab4b5955d083f03cad5
bladedurman <vedantvgrover@gmail.com>
2022-05-18 13:27:38 -0700

    Added TODO

--- Runner.java
+++ Runner.java
@@ -1,5 +1,30 @@
 public class Runner {
     public static void main(String[] args) {
-      System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        /**
+         * TODO:
+         * Necessary Stuff
+         * - Create Admin Class
+         * - Allow robot to communicate and send applications to reviewers
+         * - Create GUI/Website tab for applying for tutors.
+         * - Account creation will ask for all the user background in tutoring
+         * - Link to DB. Create user logins for customers.
+         * 
+         * - Create customer class
+         * - Allow user to create an account and apply for a tutor.
+         * - Account setup will ask for necessary info. (@AshGampa see if you can figure
+         * out how to get login to work in WIX)
+         * - Find all tutors in DB that will work. (ONLY TUTORS. NOT ALL CUSTOMERS)
+         * - Show tutors to person and let them select
+         * 
+         * Later Stuff
+         * - Set up payment methods
+         * - Set up discord server
+         * - Create discord bot for moderation and suggestions
+         * 
+         * This is a lot of stuff but I think with three people working on it, we should
+         * be able to finish this in time. At least fot he soft launch.
+         */
     }
 }
