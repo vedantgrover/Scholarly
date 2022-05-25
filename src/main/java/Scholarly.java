9a7d38d644a96d59244f22387b43d6b31f0fdb51
bladedurman <vedantvgrover@gmail.com>
2022-05-25 15:19:37 -0700

    Created the "Brain" file

---
+++ src/main/java/Scholarly.java
@@ -0,0 +1,14 @@
+public class Scholarly {
+    
+    public static void run() {
+
+    }
+
+    public static void login() {
+        
+    }
+
+    public static void register() {
+        
+    }
+}
