3319b7abc5b4a6bf5dc37ebbf91669ac281e1526
Jericoz-JC <chrisgems5@gmail.com>
2022-06-01 11:25:43 -0700

    lolol

--- src/main/java/GUIStuff/TutorApply.java
+++ src/main/java/GUIStuff/TutorApply.java
@@ -10,13 +10,25 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class TutorApply implements ActionListener {
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+import VAC.MongoDB;
 
-    JFrame frame = new JFrame();
+    
+
+public class TutorApply implements ActionListener {
+    private static MongoDB db = ScholarlyFrame.db;
+    private static JPanel descriptionPanel = new JPanel();
+    
+
+    JFrame frame = new JFrame(); 
+    JPanel panel = new JPanel();
     JLabel label = new JLabel(
             "Type your reasons and an Administrator at your organization can later connect and approve your approval. ");
     JTextField textField = new JTextField();
@@ -31,14 +43,17 @@ public class TutorApply implements ActionListener {
         label.setBounds(0, 0, 100, 50);
         label.setFont(new Font(null, Font.PLAIN, 20));
 
-        textArea.setPreferredSize(new Dimension(250, 40));
+        //textArea.setPreferredSize();
+
+        panel.setPreferredSize(new Dimension(250, 40));
 
         buttonLol.addActionListener(this);
-        buttonLol.setPreferredSize(new Dimension(150,40));
+        buttonLol.setPreferredSize(new Dimension(200,40));
 
+        
+        scrollableText.add(panel);
         scrollableText.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollableText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        
 
         
 
@@ -61,7 +76,14 @@ public class TutorApply implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == buttonLol) {
-            
+            String tempVar = textArea.getText();
+            if (tempVar.length()>151) {
+                JOptionPane.showMessageDialog(frame,"Limit to 150 charecters");
+            }
+            if (tempVar.length()<151) {
+                db.makeTutorRequest(WelcomeFrame.username.getText(), tempVar);
+                frame.dispose();
+            }
         }
     }
 
